7f8863bedf7e92d966e4de6a4bfb80b551d9db89
Shubham Rakhecha <49132205+Skysparko@users.noreply.github.com>
2023-05-04 02:38:41 +0000

    Update functions.ts

--- client/src/utils/functions.ts
+++ client/src/utils/functions.ts
@@ -12,7 +12,7 @@ export const getCookie = (cookieName: string) => {
 //axios instance method for api requests
 export const instance = axios.create({
   // baseURL: `https://sparko-ecom.shubhamrakhecha.repl.co/api/v1/`,
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://4kgh31-8080.csb.app/api/v1",
   headers: {
     authorization: `Bearer ${getCookie("bearerToken")}`,
   },
